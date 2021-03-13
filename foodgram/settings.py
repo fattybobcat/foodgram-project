b78d368baa0e490f1a7814ce0c311bb090ff1579
Aleksandr <fattybobcat@gmail.com>
2021-03-13 19:19:38 +0300

    3.46

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -69,7 +69,7 @@ DATABASES = {
     'default': {
         'ENGINE': os.environ.get('DB_ENGINE',
                                  'django.db.backends.postgresql'),
-        'NAME': os.environ.get('DB_NAME'),
+        'NAME': os.environ.get('POSTGRES_DB'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
